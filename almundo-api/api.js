463e7ef3df285f9cb72fde9a18544c81fca357bd
daguiheso <daguiheso@gmail.com>
2018-02-03 00:29:08 -0500

    Add query validation by stars and name

--- almundo-api/api.js
+++ almundo-api/api.js
@@ -2,19 +2,34 @@
 
 const debug = require('debug')('almundo:api:routes')
 const express = require('express')
+var _ = require('underscore')
 
 const api = express.Router()
 
 const data = require('./data.json')
 
+// Route GET /hotels
 api.get('/hotels', (req, res, next) => {
-  debug('A request has come to /hotels')
-  const query = req.query
 
-  // if (query.p !== 'yyy') {
-  //   return next(new Error('Agent not found'))
-  // }
-  res.send(data)
+  const query = req.query
+  // If there is a query
+  if (query !== {}) {
+    if (query.name) {
+      let re = new RegExp(query.name, 'i')
+      let predicated = []
+      data.map(obj => {
+        obj.name.match(re) ? predicated.push(obj) : null
+      })
+      return res.status(200).send({ data: predicated })
+    }
+    if (query.stars) {
+      let filtered = _.where(data, { stars: parseInt(query.stars) })
+      return res.status(200).send({ data: filtered })
+    }
+  }
+  // If there is no query
+  res.status(200).send(data)
+
 })
 
 module.exports = api
